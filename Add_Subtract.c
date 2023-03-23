5932a400f308712961025b4f3ce91e505c66018d
ayush554 <ayushbhardwaj.554@gmail.com>
2023-03-22 21:25:20 -0500

    Added add and subtract

--- Add_Subtract.c
+++ Add_Subtract.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
-void myfunction2(){
-    printf("this is source 2\n");
+int add(int num1, int num2){
+    int add= num1+num2;
+    return add;
+}
+
+int subtract(int num1, int num2){
+    int subtract= num1-num2;
+    return subtract;
 }
